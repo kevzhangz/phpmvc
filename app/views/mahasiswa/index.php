9796db5223b967c3404d9e5ef9eba3dbc61d2698
kevzhangz <kevzhangz888@gmail.com>
2021-08-09 13:27:18 +0700

    Mengubah tombol Tambah Data menjadi Tambah Data Mahasiswa

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -74,7 +74,7 @@
         </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Tambah Data</button>
+        <button type="submit" class="btn btn-primary">Tambah Data Mahasiswa</button>
       </div>
             </form>
     </div>
