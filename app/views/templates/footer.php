65e5f35cca7f20b87799f0164f09a85382aa4bb1
kevzhangz <kevzhangz888@gmail.com>
2021-08-10 12:06:38 +0700

    remove local bootstrap css and js

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -6,7 +6,7 @@
   src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
   integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" 
   crossorigin="anonymous"></script>
-<script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
 <script src="<?= BASEURL; ?>/js/script.js"></script>
 </body>
 </html>
